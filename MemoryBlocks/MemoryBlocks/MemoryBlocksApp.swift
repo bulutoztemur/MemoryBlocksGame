4488bd0c46d691dce0f2de380ca4a80372d9327f
Bulut Oztemur <bulut.oztemur@adevinta.com>
2024-08-21 13:21:49 +0200

    Init commit

---
+++ MemoryBlocks/MemoryBlocks/MemoryBlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryBlocksApp.swift
+//  MemoryBlocks
+//
+//  Created by bulut.oztemur on 21.08.24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryBlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
