5eccd597b7002a4bc698e28259d9c0fe270ddcbb
Bulut Oztemur <bulut.oztemur@adevinta.com>
2024-08-26 10:53:52 +0200

    Main page created

--- MemoryBlocks/MemoryBlocks/ContentView.swift
+++ MemoryBlocks/MemoryBlocks/ContentView.swift
@@ -7,18 +7,38 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
 #Preview {
     ContentView()
 }
+
+struct ContentView: View {
+    // Define a simple array of data
+    let items = ["2x2", "2x3", "2x4", "4x6"]
+    
+    // Define the grid layout with 2 columns
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        // Use a LazyVGrid to arrange items in a grid
+        GeometryReader { geometry in
+            LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(items, id: \.self) { item in
+                            Button {
+                                // Perform action here
+                            } label: {
+                                Text("\(item)")
+                                    .frame(width: geometry.size.width * 0.4, height: geometry.size.width * 0.4)
+                                    .background(Color.blue)
+                                    .foregroundColor(.black)
+                            }
+                            
+                        }
+            }
+            .padding()
+        }
+    }
+}
+
---
+++ MemoryBlocks/MemoryBlocks/GameBoardView.swift
@@ -0,0 +1,18 @@
+//
+//  GameBoardView.swift
+//  MemoryBlocks
+//
+//  Created by bulut.oztemur on 21.08.24.
+//
+
+import SwiftUI
+
+struct GameBoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameBoardView()
+}
