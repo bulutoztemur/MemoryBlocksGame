14833a74f0934dbba02909cd8363118866297cb1
Bulut Oztemur <bulut.oztemur@adevinta.com>
2024-09-13 09:05:10 +0200

    Move logic to ViewModel

--- MemoryBlocks/MemoryBlocks/GameBoardView+ViewModel.swift
+++ MemoryBlocks/MemoryBlocks/GameBoardView+ViewModel.swift
@@ -10,18 +10,66 @@ import Foundation
 extension GameBoardView {
     @Observable
     class ViewModel {
-        var gameBoard: GameBoard
-        var cardItems: [CardItem] = []
+        private var gameBoard: GameBoard
+        private(set) var cardViews: [CardView] = []
+        
+        private var firstCardIndex: Int?
+        private var secondCardIndex: Int?
+        
+        var gameBoardEnable: Bool {
+            secondCardIndex == nil
+        }
         
         init(gameBoard: GameBoard) {
             self.gameBoard = gameBoard
             
             for i in 0..<(gameBoard.column * gameBoard.row / 2) {
-                cardItems.append(CardItem(card: Card(rawValue: i) ?? Card.bicycle))
-                cardItems.append(CardItem(card: Card(rawValue: i) ?? Card.bicycle))
+                cardViews.append(CardView(cardItem: CardItem(card: Card(rawValue: i) ?? Card.bicycle)))
+                cardViews.append(CardView(cardItem: CardItem(card: Card(rawValue: i) ?? Card.bicycle)))
             }
             
-            cardItems.shuffle()
+            cardViews.shuffle()
+        }
+        
+        func checkCardOrder(_ index: Int) {
+            if firstCardIndex == nil {
+                firstCardIndex = index
+            } else if secondCardIndex == nil {
+                secondCardIndex = index
+            } else {
+                fatalError("EXCEPTIONAL CASE")
+            }
+        }
+        
+        private func reset() {
+            firstCardIndex = nil
+            secondCardIndex = nil
+        }
+        
+        private func flipOverUnmatchedCards() {
+            cardViews[firstCardIndex!].flipCard()
+            cardViews[secondCardIndex!].flipCard()
+        }
+        
+        private func cardsUnmatched() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.flipOverUnmatchedCards()
+                self.reset()
+            }
+        }
+        
+        private func cardsMatched() {
+            self.reset()
+        }
+        
+        func checkMatch() {
+            if let firstCardIndex, let secondCardIndex {
+                if cardViews[firstCardIndex].cardItem.card.rawValue == cardViews[secondCardIndex].cardItem.card.rawValue {
+                    cardsMatched()
+                } else {
+                    cardsUnmatched()
+                }
+            }
         }
     }
 }
--- MemoryBlocks/MemoryBlocks/GameBoardView.swift
+++ MemoryBlocks/MemoryBlocks/GameBoardView.swift
@@ -13,73 +13,31 @@ struct GameBoardView: View {
     var columns: [GridItem] {
         [GridItem](repeating: GridItem(.flexible()), count: gameBoard.column)
     }
-    
-    @State private var firstCardIndex: Int?
-    @State private var secondCardIndex: Int?
-    
+        
     @State private var viewModel: ViewModel
-    
-    private var cardViews: [CardView] = []
-            
-    var index: Int?
-    
+                    
     init(gameBoard: GameBoard) {
         self.gameBoard = gameBoard
         self.viewModel = ViewModel(gameBoard: gameBoard)
-        self.cardViews = self.viewModel.cardItems.map { CardView(cardItem: $0) }
     }
     
     var body: some View {
         GeometryReader { geometry in
             ScrollView(.vertical, showsIndicators: false) {
                 LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(0..<cardViews.count, id: \.self) { index in
-                        cardViews[index]
+                    ForEach(0..<viewModel.cardViews.count, id: \.self) { index in
+                        viewModel.cardViews[index]
                             .frame(width: gameBoard.cardSize, height: gameBoard.cardSize)
                             .simultaneousGesture(TapGesture().onEnded {
-                                if firstCardIndex == nil {
-                                    firstCardIndex = index
-                                } else if secondCardIndex == nil {
-                                    secondCardIndex = index
-                                } else {
-                                    fatalError("EXCEPTIONAL CASE")
-                                }
-                                
-                                if let firstCardIndex, let secondCardIndex {
-                                    if cardViews[firstCardIndex].cardItem.card.rawValue == cardViews[secondCardIndex].cardItem.card.rawValue {
-                                        cardsMatched()
-                                    } else {
-                                        cardsUnmatched()
-                                    }
-                                }
+                                viewModel.checkCardOrder(index)
+                                viewModel.checkMatch()
                             })
                     }
                 }
                 .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
-                .allowsHitTesting(secondCardIndex == nil)
+                .allowsHitTesting(viewModel.gameBoardEnable)
             }
         }
-    }
-    
-    func flipOverUnmatchedCards() {
-        self.cardViews[firstCardIndex!].flipCard()
-        self.cardViews[secondCardIndex!].flipCard()
-    }
-    
-    func reset() {
-        self.firstCardIndex = nil
-        self.secondCardIndex = nil
-    }
-    
-    func cardsUnmatched() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.flipOverUnmatchedCards()
-            self.reset()
-        }
-    }
-    
-    func cardsMatched() {
-        self.reset()
     }
 }
 
