e4d7f93e445f4884896230f08798a5ae744910f7
Bulut Oztemur <bulutoztemur@gmail.com>
2024-12-30 13:49:38 +0300

    Update GameBoard.swift

--- MemoryBlocks/MemoryBlocks/Views/GameBoard/GameBoard.swift
+++ MemoryBlocks/MemoryBlocks/Views/GameBoard/GameBoard.swift
@@ -11,7 +11,9 @@ import UIKit
 enum GameBoard: CaseIterable {
     case twotwo
     case threetwo
+    case fourthree
     case fourfour
+    case fivefour
     case sixfour
     case sixfive
     case eightfive
@@ -22,8 +24,12 @@ enum GameBoard: CaseIterable {
             2
         case .threetwo:
             3
+        case .fourthree:
+            4
         case .fourfour:
             4
+        case .fivefour:
+            5
         case .sixfour:
             6
         case .sixfive:
@@ -39,7 +45,11 @@ enum GameBoard: CaseIterable {
             2
         case .threetwo:
             2
+        case .fourthree:
+            3
         case .fourfour:
+            4
+        case .fivefour:
             4
         case .sixfour:
             4
@@ -61,8 +71,12 @@ enum GameBoard: CaseIterable {
             "2x2"
         case .threetwo:
             "3x2"
+        case .fourthree:
+            "4x3"
         case .fourfour:
             "4x4"
+        case .fivefour:
+            "5x4"
         case .sixfour:
             "6x4"
         case .sixfive:
